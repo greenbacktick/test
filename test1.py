471073e775f4a85ff59fbbc856ae7ca215146390
Loick Pelet <peletloick@sysdedi.com>
2014-05-21 23:57:32 -0400

    got it working

--- test1.py
+++ test1.py
@@ -4,48 +4,57 @@ import re
 
 class Mac:
     'Common class to represente MAC'
-    
+
     def __init__(self,mac,formac_src,formac_dst):
         self.mac = mac.lower()
         self.formac_src = formac_src.upper()
         self.formac_dst = formac_dst.upper()
-
+    
     def displayMac(self):
-        print "MAC",self.mac, "format : ",self.formac_src, "to format : ", self.formac_dst
-
+        mac = self.changeFormat()
+        print "MAC", mac, "format : ", self.formac_src, "to format : ", self.formac_dst
+    
     def changeFormat(self):
+        mac = self.cleanMac()
         if self.formac_dst == 'CISCO':
-            self.mac_dst = self.mac[:4],self.mac[4:8],self.mac[8:]
-            print ".".join(self.mac_dst)
+            mac_dst = ".".join((mac[:4],mac[4:8],mac[8:]))
         elif self.formac_dst == 'PLAIN':
-            self.mac_dst = self.mac.lower()
-            print self.mac_dst
+            mac_dst = mac.lower()
         elif self.formac_dst == 'IEEE':
-            self.mac_dst = self.mac[:2],self.mac[2:4],self.mac[4:6],self.mac[6:8],self.mac[8:10],self.mac[10:]
-            print ":".join(self.mac_dst)
+            mac_dst = ":".join((mac[:2],mac[2:4],mac[4:6],mac[6:8],mac[8:10],mac[10:]))
         elif self.formac_dst == 'DASH':
-            self.mac_dst = self.mac[:2],self.mac[2:4],self.mac[4:6],self.mac[6:8],self.mac[8:10],self.mac[10:]
-            print "-".join(self.mac_dst)
-        else :
+            mac_dst = "-".join((mac[:2],mac[2:4],mac[4:6],mac[6:8],mac[8:10],mac[10:]))
+        else:
             print "Error source format unknown"
-
+        return mac_dst
+    
     def cleanMac(self):
         p = re.compile( '[0-9a-fA-F]+')
         cleaned_mac = ''.join(p.findall(self.mac))
-        return cleaned_mac
-
-    def printMac(self):
-        cleaned_mac = self.cleanMac()
-        print cleaned_mac
+        if len(cleaned_mac) != 12:
+            print "error : Invalid given MAC address"
+            exit()
+        else:
+            return cleaned_mac
 
 mac1 = Mac("001122334455","plain","ieee")
-mac2 = Mac("66:77:88:99:00:11","ieee","ieee")
+mac2 = Mac("66:77:88:99:00:11","ieee","dash")
 mac3 = Mac("00:AA:11:BB:22:CC","pf","cisco")
+mac4 = Mac("30:af:11:cb:46:dc","pf","plain")
+mac5 = Mac("adbc54na22ce","pf","plain")
 
-#mac1.displayMac()
+mac1.displayMac()
+mac2.displayMac()
+mac3.displayMac()
+mac4.displayMac()
+mac5.displayMac()
 #mac1.changeFormat()
 #mac2.displayMac()
 #mac2.changeFormat()
 #mac3.displayMac()
 #mac3.changeFormat()
-mac2.printMac()
+#mac1.changeFormat()
+#mac2.changeFormat()
+#mac3.changeFormat()
+#mac4.changeFormat()
+#mac5.changeFormat()
